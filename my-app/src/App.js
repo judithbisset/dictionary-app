a3c805408d1eead17b4b333e7e8524f77be48525
judithbisset <77537313+judithbisset@users.noreply.github.com>
2021-06-29 15:48:37 +0200

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+       <button className="btn btn-primary w-80">
+          Search
+        </button>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

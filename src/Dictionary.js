10dcb5ee62a6762be37ea19b261b9179ba9a6c94
judithbisset <77537313+judithbisset@users.noreply.github.com>
2021-06-30 13:36:02 +0200

    integrated dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary() {
+    
     let [keyword, setKeyword] = useState("");
+    function handleResponse(response) {
+        console.log(response.data[0]);
+
+    }
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+       
+        let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
+        axios.get(apiURL).then(handleResponse);
     }
 return(
     <div className="Dictionary">

3c22a9e873ff367cbb64f1d0f00841cf74add17d
judithbisset <77537313+judithbisset@users.noreply.github.com>
2021-06-30 13:06:15 +0200

    created the search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 
 import './App.css';
+import React from "react";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+       <div className="container">
+      
         
-       <button className="btn btn-primary w-80">
-          Search
-        </button>
-        
-      </header>
+       <p>Dictionary App</p>
+          
+        <Dictionary />
+     
+      <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/judith-maier-bisset-4943b198/" target="_blank" rel="noreferrer">
+            Judith Maier Bisset
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/judithbisset/dictionary-app" rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>{" "}
+          
+          
+        </footer>
+    </div>
     </div>
   );
 }

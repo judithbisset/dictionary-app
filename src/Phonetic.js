9252335acdfe6c600f4f8997f3d90ef5c5fe8a12
judithbisset <77537313+judithbisset@users.noreply.github.com>
2021-07-06 14:06:48 +0200

    included phonetics component

---
+++ src/Phonetic.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Phonetic(props) {
+    return (
+        <div className="Phonetic">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
+            <li>{props.phonetic.text}</li>
+        </div>
+    )
+}
